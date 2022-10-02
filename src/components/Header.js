85714e94b6e033861a6bfeca0ecc5b02b34a2437
emericgautier <emericgtr@gmail.com>
2022-10-02 11:25:37 +0200

    Update Header.js component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <div>
-      <h1>HEADER</h1>
-    </div>
+    <header>
+      <div className="header-content">
+        <h1>
+          La montagne <br /> ça vous gagne
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <span>
+          <a href="#main">
+            <i className="fa-solid fa-arrow-down"></i>
+          </a>
+        </span>
+      </div>
+      <Navbar />
+    </header>
   );
 };
 
